df4cae69c7a037b6a28dcbbfdb9e2bde0f324142
Adam Owada <adamowada@gmail.com>
2020-06-04 21:34:12 -0700

    fibonacci numbers function works and tests well

--- math_series/series.py
+++ math_series/series.py
@@ -1,3 +1,9 @@
 def fibonacci(n):
-  return n
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    count = fibonacci(n-1) + fibonacci(n-2)
+    return count
+
 
--- tests/test_series.py
+++ tests/test_series.py
@@ -3,10 +3,40 @@ from math_series.series import fibonacci
 
 
 def test_version():
-  assert __version__ == '0.1.0'
+    assert __version__ == '0.1.0'
 
-def test_fibonacci():
-  actual = fibonacci(15)
-  expected = 15
-  assert actual == expected
 
+def test_fibonacci1():
+    actual = fibonacci(0)
+    expected = 0
+    assert actual == expected
+
+
+def test_fibonacci2():
+    actual = fibonacci(1)
+    expected = 1
+    assert actual == expected
+
+
+def test_fibonacci3():
+    actual = fibonacci(2)
+    expected = 1
+    assert actual == expected
+
+
+def test_fibonacci4():
+    actual = fibonacci(3)
+    expected = 2
+    assert actual == expected
+
+
+def test_fibonacci5():
+    actual = fibonacci(4)
+    expected = 3
+    assert actual == expected
+
+
+def test_fibonacci6():
+    actual = fibonacci(5)
+    expected = 5
+    assert actual == expected
