6f2365574e141b2fa6338d4b1a4c8ca067f55407
Adam Owada <adamowada@gmail.com>
2020-06-04 20:39:08 -0700

    set up tests proof of life

--- tests/test_series.py
+++ tests/test_series.py
@@ -1,5 +1,12 @@
 from math_series import __version__
+from math_series.series import fibonacci
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+  assert __version__ == '0.1.0'
+
+def test_fibonacci():
+  actual = fibonacci(15)
+  expected = 15
+  assert actual == expected
+
